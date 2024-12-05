e75d64e35036252fbefe73b960ade01823bdf828
Kaushik, Udit <ukaushik@ebay.com>
2024-12-04 22:22:50 -0800

    day 5 part 2

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -7,7 +7,6 @@ use tokio::{fs, join};
 
 pub async fn day5() {
     let (map, updates) = join!(build_map(), build_updates());
-
     let mut count = 0;
     let mut count2 = 0;
     for update in updates.iter() {
@@ -16,8 +15,8 @@ pub async fn day5() {
             count += update[mid].parse::<usize>().unwrap();
         } else {
             let update = fix(&map, &update);
-            // let mid = update.len() / 2;
-            // count2 += update[mid].parse::<usize>().unwrap();
+            let mid = update.len() / 2;
+            count2 += update[mid].parse::<usize>().unwrap();
         }
     }
     println!("AOC DAY 5 Part 1: {count}");
@@ -25,7 +24,7 @@ pub async fn day5() {
 }
 
 async fn build_map() -> HashMap<String, HashSet<String>> {
-    let buf = fs::read("./src/day5/sample_map.txt").await.unwrap();
+    let buf = fs::read("./src/day5/real_map.txt").await.unwrap();
 
     let content: std::borrow::Cow<'_, str> = String::from_utf8_lossy(&buf);
     let mut content: Vec<&str> = content.split("\n").collect();
@@ -42,7 +41,7 @@ async fn build_map() -> HashMap<String, HashSet<String>> {
 }
 
 async fn build_updates() -> Vec<Vec<String>> {
-    let buf = fs::read("./src/day5/sample_updates.txt").await.unwrap();
+    let buf = fs::read("./src/day5/real_updates.txt").await.unwrap();
 
     let content: std::borrow::Cow<'_, str> = String::from_utf8_lossy(&buf);
     let mut content: Vec<&str> = content.split("\n").collect();
@@ -82,6 +81,32 @@ fn check(map: &HashMap<String, HashSet<String>>, updates: &Vec<String>) -> bool 
 }
 
 fn fix(map: &HashMap<String, HashSet<String>>, updates: &Vec<String>) -> Vec<String> {
-    println!("updates: {:?}", updates);
-    Vec::new()
+    let mut in_degrees: Vec<(&String, usize)> = Vec::new();
+
+    for (i, u) in updates.iter().enumerate() {
+        let deps = map.get(u);
+        match deps {
+            None => in_degrees.push((u, 0)),
+            Some(deps) => {
+                let mut in_degree = 0;
+                for d in deps.iter() {
+                    let p = updates.iter().position(|x| *x == d.to_string());
+                    match p {
+                        Some(_) => {
+                            in_degree += 1;
+                        }
+                        None => continue,
+                    }
+                }
+                in_degrees.push((u, in_degree));
+            }
+        }
+    }
+
+    in_degrees.sort_by(|(_, a), (_, b)| a.cmp(b));
+
+    in_degrees
+        .iter()
+        .map(|x| x.0.to_string())
+        .collect::<Vec<String>>()
 }
